bc94237e5e71b44759bd4da66c0d53568332ebb6
Tanmay Patil <tanmayprafullapatil@gmail.com>
2026-09-23 11:50:59 +0530

    feat(network): configure Gson response conversion
    Register GsonConverterFactory with Retrofit to deserialize JSON API responses while retaining scalar conversion for the server status endpoint.

--- app/src/main/java/com/tanmay/lumo/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/tanmay/lumo/data/remote/RetrofitClient.kt
@@ -3,6 +3,7 @@ package com.tanmay.lumo.data.remote
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import com.tanmay.lumo.BuildConfig
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
@@ -12,6 +13,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
     }
