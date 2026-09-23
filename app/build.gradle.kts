5ed6baa98726dd1097bb40615e34574cdde0f651
Tanmay Patil <tanmayprafullapatil@gmail.com>
2026-09-23 11:48:20 +0530

    build(network): add Gson converter dependency
    Add Retrofit Gson converter support for deserializing JSON API responses into Kotlin data models.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.scalars)
+    implementation(libs.retrofit.converter.gson)
 }
