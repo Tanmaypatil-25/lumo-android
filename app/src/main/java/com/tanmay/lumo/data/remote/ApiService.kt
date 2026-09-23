9f8a4ba10d40e8de6e01b165b1f6f446892681a6
Tanmay Patil <tanmayprafullapatil@gmail.com>
2026-09-23 11:42:27 +0530

    feat(auth): add login API endpoint
    Add the Retrofit login request using LoginRequest and AuthResponse models to connect the Android client with the existing Lumo authentication API.

--- app/src/main/java/com/tanmay/lumo/data/remote/ApiService.kt
+++ app/src/main/java/com/tanmay/lumo/data/remote/ApiService.kt
@@ -1,10 +1,20 @@
 package com.tanmay.lumo.data.remote
 
+import com.tanmay.lumo.data.model.AuthResponse
+import com.tanmay.lumo.data.model.LoginRequest
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
     @GET("api/status")
     suspend fun getServerStatus(): Response<String>
+
+    @POST("api/auth/login")
+    suspend fun login(
+        @Body request: LoginRequest
+    ): Response<AuthResponse>
+
 }
